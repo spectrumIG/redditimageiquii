c21e99a3df408dbd30d5c0ff5d456b434f023d44
Dani <cloudsky.it@gmail.com>
2020-07-16 22:21:01 +0200

    Adds first draft for fragment and navigation

---
+++ app/src/main/java/it/iquii/test/reddit/photogrid/PhotoGridFragment.kt
@@ -0,0 +1,23 @@
+package it.iquii.test.reddit.photogrid
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import it.iquii.test.reddit.R
+import it.iquii.test.reddit.databinding.PhotoGridFragmentBinding
+
+@AndroidEntryPoint
+class PhotoGridFragment : Fragment(R.layout.photo_grid_fragment) {
+
+    private var fragmentBindings : PhotoGridFragmentBinding? = null
+    private val viewModel : PhotoGridViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentBindings = PhotoGridFragmentBinding.bind(view)
+
+
+    }
+}
---
+++ app/src/main/java/it/iquii/test/reddit/photogrid/PhotoGridViewModel.kt
@@ -0,0 +1,7 @@
+package it.iquii.test.reddit.photogrid
+
+import androidx.lifecycle.ViewModel
+
+class PhotoGridViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
